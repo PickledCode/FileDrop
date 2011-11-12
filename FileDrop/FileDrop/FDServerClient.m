c8062b3df447478c10fe897b94ab75286e78dcf0
Ryan <rsully.dev+git@me.com>
2011-11-12 15:48:58 -0500

    Had Xcode re-indent using spaces

--- FileDrop/FileDrop/FDServerClient.m
+++ FileDrop/FileDrop/FDServerClient.m
@@ -52,93 +52,93 @@
         
         while(![[NSThread currentThread] isCancelled] && [socket isOpen]) {
             @autoreleasepool {
-				NSLog(@"Starting read...");
-				id obj = [KBPacket readPacketFromSocket:socket];
-				if (![obj isKindOfClass:[NSDictionary class]]) {
-					continue;
-				}
-				
-				NSDictionary *dict = (NSDictionary*)obj;
-				NSString *dType = [dict objectForKey:@"type"];
-				
-				if ([dType isEqualToString:@"conn"]) {
-					// Action and possible start info
-					NSString *dAction = [dict objectForKey:@"action"];
-					NSLog(@"Conn change: %@", dAction);
-					
-					if ([dAction isEqualToString:@"connected"]) {
-						isConnected = YES;
-						// On connect, any files in our list need to request again and assume fresh state
-						for (FDFileSend *file in [fileManager filesInSection:FDFM_FILESEND_SECTION]) {
-							file.bytesTransfered = 0;
-							file.isAccepted = NO;
-							[KBPacket writeInitFile:file toSocket:socket];
-						}
-						[self uploadThreadBegin];
-					} else if ([dAction isEqualToString:@"disconnected"]) {
-						[self uploadThreadFinish];
-						isConnected = NO;
-					}
-					
-				} else if ([dType isEqualToString:@"error"]) {
-					// Message and error code
-					NSString *dMsg = [dict objectForKey:@"msg"];
-					NSNumber *dCode = [dict objectForKey:@"code"];
-					
-					NSLog(@"KBProxyServer ERROR: %d: %@", [dCode intValue], dMsg);
-					if ([dCode intValue] == 1) {
-						[fileManager.delegate fileManagerErrorTokenInvalid:fileManager];
-					}
-					
-				} else if ([dType isEqualToString:@"data"]) {
-					
-					NSDictionary *dDict = [dict objectForKey:@"data"];
-					NSString *fType = [dDict objectForKey:@"type"];
-					NSString *fAct = [dDict objectForKey:@"action"];
-					NSString *fID = [dDict objectForKey:@"id"];
-					
-					if ([fType isEqualToString:@"file"]) {
-						if ([fAct isEqualToString:@"data"]) {
-							FDFile *_file = [fileManager getFileRecvFromID:fID];
-							if (!_file) {
-								NSLog(@"File doesn't exist but we have ID/data.");
-								continue;
-							} else if (!_file.localPath) {
-								NSLog(@"File doesn't have path: %@", _file);
-								continue;
-							}
-							
-							NSData *_data = [dDict objectForKey:@"data"];
-							NSLog(@"Saving to %@", _file.localPath);
-							NSFileHandle *handler = _file.fileHandler;
-							[handler seekToFileOffset:_file.bytesTransfered];
-							[handler writeData:_data];
-							_file.bytesTransfered = _file.bytesTransfered + [_data length];
-							
-						} else if ([fAct isEqualToString:@"init"]) {
-							NSLog(@"-init new file from meta");
-							NSMutableDictionary *_meta = [NSMutableDictionary dictionaryWithDictionary:[dDict objectForKey:@"meta"]];
-							[_meta setObject:fID forKey:@"id"];
-							[fileManager addFileFromMeta:_meta];
-						} else if ([fAct isEqualToString:@"accept"]) {
-							NSLog(@"-accepted new file");
-							FDFile *_file = [fileManager getFileSendFromID:fID];
-							_file.isAccepted = YES;
-						} else if ([fAct isEqualToString:@"decline"]) {
-							NSLog(@"-declined new file");
-							
-						} else if ([fAct isEqualToString:@"cancel"]) {
-							NSLog(@"-canceled new file: %@", dDict);
-							
-						} else if ([fAct isEqualToString:@"update"]) {
-							NSLog(@"-updating file for bytes %@", dDict);
-							FDFile *_file = [fileManager getFileSendFromID:fID];
-							NSUInteger newTrans = [[dDict objectForKey:@"bytesTransfered"] unsignedIntegerValue];
-							_file.bytesTransfered = newTrans;
-							
-						}
-					}
-				}
+                NSLog(@"Starting read...");
+                id obj = [KBPacket readPacketFromSocket:socket];
+                if (![obj isKindOfClass:[NSDictionary class]]) {
+                    continue;
+                }
+                
+                NSDictionary *dict = (NSDictionary*)obj;
+                NSString *dType = [dict objectForKey:@"type"];
+                
+                if ([dType isEqualToString:@"conn"]) {
+                    // Action and possible start info
+                    NSString *dAction = [dict objectForKey:@"action"];
+                    NSLog(@"Conn change: %@", dAction);
+                    
+                    if ([dAction isEqualToString:@"connected"]) {
+                        isConnected = YES;
+                        // On connect, any files in our list need to request again and assume fresh state
+                        for (FDFileSend *file in [fileManager filesInSection:FDFM_FILESEND_SECTION]) {
+                            file.bytesTransfered = 0;
+                            file.isAccepted = NO;
+                            [KBPacket writeInitFile:file toSocket:socket];
+                        }
+                        [self uploadThreadBegin];
+                    } else if ([dAction isEqualToString:@"disconnected"]) {
+                        [self uploadThreadFinish];
+                        isConnected = NO;
+                    }
+                    
+                } else if ([dType isEqualToString:@"error"]) {
+                    // Message and error code
+                    NSString *dMsg = [dict objectForKey:@"msg"];
+                    NSNumber *dCode = [dict objectForKey:@"code"];
+                    
+                    NSLog(@"KBProxyServer ERROR: %d: %@", [dCode intValue], dMsg);
+                    if ([dCode intValue] == 1) {
+                        [fileManager.delegate fileManagerErrorTokenInvalid:fileManager];
+                    }
+                    
+                } else if ([dType isEqualToString:@"data"]) {
+                    
+                    NSDictionary *dDict = [dict objectForKey:@"data"];
+                    NSString *fType = [dDict objectForKey:@"type"];
+                    NSString *fAct = [dDict objectForKey:@"action"];
+                    NSString *fID = [dDict objectForKey:@"id"];
+                    
+                    if ([fType isEqualToString:@"file"]) {
+                        if ([fAct isEqualToString:@"data"]) {
+                            FDFile *_file = [fileManager getFileRecvFromID:fID];
+                            if (!_file) {
+                                NSLog(@"File doesn't exist but we have ID/data.");
+                                continue;
+                            } else if (!_file.localPath) {
+                                NSLog(@"File doesn't have path: %@", _file);
+                                continue;
+                            }
+                            
+                            NSData *_data = [dDict objectForKey:@"data"];
+                            NSLog(@"Saving to %@", _file.localPath);
+                            NSFileHandle *handler = _file.fileHandler;
+                            [handler seekToFileOffset:_file.bytesTransfered];
+                            [handler writeData:_data];
+                            _file.bytesTransfered = _file.bytesTransfered + [_data length];
+                            
+                        } else if ([fAct isEqualToString:@"init"]) {
+                            NSLog(@"-init new file from meta");
+                            NSMutableDictionary *_meta = [NSMutableDictionary dictionaryWithDictionary:[dDict objectForKey:@"meta"]];
+                            [_meta setObject:fID forKey:@"id"];
+                            [fileManager addFileFromMeta:_meta];
+                        } else if ([fAct isEqualToString:@"accept"]) {
+                            NSLog(@"-accepted new file");
+                            FDFile *_file = [fileManager getFileSendFromID:fID];
+                            _file.isAccepted = YES;
+                        } else if ([fAct isEqualToString:@"decline"]) {
+                            NSLog(@"-declined new file");
+                            
+                        } else if ([fAct isEqualToString:@"cancel"]) {
+                            NSLog(@"-canceled new file: %@", dDict);
+                            
+                        } else if ([fAct isEqualToString:@"update"]) {
+                            NSLog(@"-updating file for bytes %@", dDict);
+                            FDFile *_file = [fileManager getFileSendFromID:fID];
+                            NSUInteger newTrans = [[dDict objectForKey:@"bytesTransfered"] unsignedIntegerValue];
+                            _file.bytesTransfered = newTrans;
+                            
+                        }
+                    }
+                }
             }
         }
     }
